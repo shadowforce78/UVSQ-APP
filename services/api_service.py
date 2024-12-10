6c7993f2778efe8c298e782cc02e7dff398a35a6
shadowforce78 <planque.adam@gmail.com>
2024-12-10 19:24:38 +0100

    Add autocomplete API endpoint and update login function to return JSON response

--- services/api_service.py
+++ services/api_service.py
@@ -3,6 +3,7 @@ import requests
 API = "http://localhost:8000"
 EDTEndpoint = "/uvsq/edt/{classe}+{start}+{end}"
 BULLETINSEndpoint = "/uvsq/bulletin/{id}+{password}"
+AUTOCOMPLETEEndpoint = "/uvsq/classe/{query}"
 
 def get_edt_data(classe, start, end):
     response = requests.get(
@@ -12,4 +13,8 @@ def get_edt_data(classe, start, end):
 
 def login(id, password):
     response = requests.get(API + BULLETINSEndpoint.format(id=id, password=password))
+    return response.json()
+
+def autocomplete(query):
+    response = requests.get(API + AUTOCOMPLETEEndpoint.format(query=query))
     return response.json()
